db6f25466f57cdf230d9f6bbc7de96ff204dee2d
Truong Nguyen <nhattruongniit@yahoo.com.vn>
2019-12-05 11:29:28 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import documentsDb from './db/documents.json';
+
 
 function App() {
+  console.log('====documentsDb==', documentsDb.data)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     this is app component dsa
     </div>
   );
 }
